51d1929bf91bfc4f8f497a64922fce09c9e45ae2
KPVBY <kpvbygm@gmail.com>
2019-11-06 16:04:43 +0300

    add pachage.json and edited html/css for todo

--- README.md
+++ README.md
@@ -10,19 +10,5 @@
 
 ## copy materials
 ```
-"devDependencies": {
-    "css-loader": "^3.2.0",
-    "file-loader": "^4.2.0",
-    "html-webpack-plugin": "^3.2.0",
-    "style-loader": "^1.0.0",
-    "webpack": "^4.41.2",
-    "webpack-cli": "^3.3.10",
-    "webpack-dev-server": "^3.9.0"
-  },
-"scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "build": "webpack --config webpack.config.js",
-    "watch": "webpack --config webpack.config.js --watch",
-    "start": "webpack-dev-server --config webpack.config.js --open"
-  },
+np
 ```
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import './style.css';
-import printMe from './print.js';
 
 function component() {
-    const wrapper = document.createElement('div');
-    
-    const info = document.createElement('div');
-    const btn = document.createElement('button');
-
-    info.innerHTML = ['Hello', 'webpack'].join(' ');
-    info.classList.add('hello');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    wrapper.appendChild(btn);
-    wrapper.appendChild(info);
-    return wrapper;
+    // const wrapper = document.createElement('div');
+    //
+    // const info = document.createElement('div');
+    // const btn = document.createElement('button');
+    //
+    // info.innerHTML = ['Hello', 'webpack'].join(' ');
+    // info.classList.add('hello');
+    //
+    // btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;
+    //
+    // wrapper.appendChild(btn);
+    // wrapper.appendChild(info);
+    // return wrapper;
 }
 
 document.body.appendChild(component());
