3ac44ae74616f3e1f332e58168cfaac140f20b1b
KPVBY <kpvbygm@gmail.com>
2019-11-28 02:55:03 +0300

    take arrayIncludes

--- src/components/input-area.js
+++ src/components/input-area.js
@@ -9,17 +9,21 @@ export default class InputArea extends Main {
         super(templateInputForSearch);
         this._listLS = App.getLocalStorageItems;
         this._lists=document.getElementsByClassName(APPMESSAGECONTENTFORLIST);
-        this._node.addEventListener('keydown', this.searchInitialHandler);
+        this._inputValue=this.node;
+        this._node.addEventListener('keyup', this.searchInitialHandler);
+        this._clear();
     }
 
     searchInitialHandler = () => {
+        this._clear();
+        const arrayIncludes=this._listLS.filter(element => element.value.toLowerCase().includes(this._inputValue.value));
+        return arrayIncludes
+        // arrayIncludes.forEach(element=>this._lists.append(element))
+    };
+
+    _clear(){
         while (this._lists.length !== 0) {
             this._lists[0].parentNode.removeChild(this._lists[0])
         }
-
-        this._listLS.filter((element, index) => {
-            console.log("hi")
-        })
-    };
-
+    }
 }
